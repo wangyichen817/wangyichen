efca64591de444fa90bcf61a780f3c65d28066d8
wangyichen <1974207572@qq.com>
2019-11-19 12:40:32 +0800

    提交代码

---
+++ 第一天作业.c
@@ -0,0 +1,50 @@
+//乘法表
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a,b;
+//	for (a = 1; a < 10; a++){
+//		for (b = 1; b < 10; b++)
+//		if (b <= a)
+//		{
+//			printf("%d*%d=%d ", a, b, a*b);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//素数
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int a, b;
+//	for (a = 100; a <= 200; a++)
+//	{
+//		for (b = 2; b <= a - 1; b++)
+//		{
+//
+//			if (a%b == 0)
+//				break;
+//			if (b == a - 1)
+//				printf("%d\n", a);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//
+//}
+//闰年
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main(){
+//	int a;
+//	for (a > 1000; a < 2001;)
+//	if (a % 4 == 0 && a % 100 !=0 || a % 400 == 0){
+//		printf("%d\n", a);
+//	}
+//	system("pause");
+//	return 0;
+//}
