089b85d14753b74ee3f5c44560bc02699b3f1b93
wangyichen <1974207572@qq.com>
2019-11-25 16:07:28 +0800

    提交代码

---
+++ 第四天作业.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//void par(star)
+//{
+//	int i = 0;
+//	for (i = 0; i <= star; i++)
+//	{
+//		for (int a = 0; a <i * 2 + 1; a++){
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	int n = 0;
+//	for (n = star; n >= 0; n--)
+//	{
+//		for (int m = 0; m < n * 2 - 1; m++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int x;
+//	scanf("%d",&x);
+//	par(x);
+//	system("pause");
+//int main()
+//{
+//	int a = 0,b=0,c=0,d=0;
+//	for (a = 100; a < 1000; a++)
+//	{
+//		b = a % 100 % 10;
+//		c = a % 100 / 10;
+//		d = a / 100;
+//		if (a == b*b*b + c*c*c + d*d*d)
+//		{
+//			printf("%d\n", a);
+//		}
+//	}
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	for (int c = 0; c < 5; c++)
+//	{
+//		a = a * 10 + a;
+//		a += a;
+//	}
+//	printf("%d", a);
+//	system("pause");
+//	return 0;
+//}
